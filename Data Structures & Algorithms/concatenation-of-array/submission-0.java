9424246d832eb38cb1f2facd5520d2045453c8e6
MINAKSHI KUMARI <minakshikarna4031@gmail.com>
2026-06-26 01:12:47 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2*n];
+
+        for(int i = 0;i<n;i++){
+            ans[i] = nums[i];
+        }
+        int c = 0;
+        for(int i = n;i<2*n;i++){
+            ans[i] = nums[c++];
+        }
+        return ans;
+    }
+}
